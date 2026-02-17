2ac180c80540af5407a242cef3de70715650b293
ArginAslanian <arginaslaniandeveloper@gmail.com>
2026-02-17 13:48:42 -0800

    exchange clean up

--- PowerShell for Exchange Online.ps1
+++ PowerShell for Exchange Online.ps1
@@ -15,10 +15,89 @@ Connect-ExchangeOnline -UserPrincipalName admin@xyz.com
 #Disconnect from Exchange Online
 Disconnect-ExchangeOnline
 
-#Get list of all mailboxes
-Get-Mailbox -ResultSize Unlimited
 
-#Get list of all distribution groups
+################################################################
+###### Mailbox Operations
+################################################################
+
+# List mailbox permissions for a specific mailbox
+Get-MailboxPermission -Identity "mailbox_email"
+
+# List send-as permissions for a specific mailbox
+Get-RecipientPermission -Identity "mailbox_email"
+
+# List send-on-behalf permissions for a specific mailbox
+Get-Mailbox -Identity "mailbox_email" | Select-Object -ExpandProperty GrantSendOnBehalfTo
+
+# Share a mailbox from one user to another with specific permissions
+Add-MailboxPermission -Identity "mailbox_email" -User "user_email" -AccessRights FullAccess
+
+# Grant send-as permission to a user for a specific mailbox
+Add-RecipientPermission -Identity "mailbox_email" -Trustee "user_email" -AccessRights SendAs
+
+# Grant send-on-behalf permission to a user for a specific mailbox
+Set-Mailbox -Identity "mailbox_email" -GrantSendOnBehalfTo "user_email"
+
+# Remove mailbox permissions for a specific mailbox
+Remove-MailboxPermission -Identity "mailbox_email" -User "user_email" -AccessRights FullAccess
+
+# Remove send-as permission for a user for a specific mailbox
+Remove-RecipientPermission -Identity "mailbox_email" -Trustee "user_email" -AccessRights SendAs
+
+# Remove send-on-behalf permission for a user for a specific mailbox
+Set-Mailbox -Identity "mailbox_email" -GrantSendOnBehalfTo $null
+
+# Edit mailbox permissions for a specific mailbox
+Set-MailboxPermission -Identity "mailbox_email" -User "user_email" -AccessRights ReadPermissions
+
+# Set automatic replies for a specific mailbox
+Set-MailboxAutoReplyConfiguration -Identity "mailbox_email" -AutoReplyState Enabled -InternalMessage "I am currently out of the office." -ExternalMessage "I am currently out of the office."
+
+# Remove automatic replies for a specific mailbox
+Set-MailboxAutoReplyConfiguration -Identity "mailbox_email" -AutoReplyState Disabled
+
+# Check automatic reply configuration for a specific mailbox
+Get-MailboxAutoReplyConfiguration -Identity "mailbox_email"
+
+# List email forwarding settings for a specific mailbox
+Get-Mailbox -Identity "mailbox_email" | Select-Object -ExpandProperty ForwardingSmtpAddress, ForwardingAddress
+
+# Set email forwarding for a specific mailbox
+Set-Mailbox -Identity "<mailbox_email>" -ForwardingSmtpAddress "forwardto@domain.com" -DeliverToMailboxAndForward $true
+
+# Remove email forwarding for a specific mailbox
+Set-Mailbox -Identity "<mailbox_email>" -ForwardingSmtpAddress $null -DeliverToMailboxAndForward $false
+
+# Get list of all shared mailboxes
+Get-Mailbox -RecipientTypeDetails SharedMailbox -ResultSize Unlimited
+
+################################################################
+###### Calendar Operations
+################################################################
+
+# List calendar permissions for a specific calendar
+Get-MailboxFolderPermission -Identity xyz@abc.com:\Calendar
+
+# Share a calendar from one user to another with specific permissions
+Add-MailboxFolderPermission -Identity "calendar_owner_email:\Calendar" -User "calendar_user_email" -AccessRights FullAccess
+
+# Share a calendar from one user to another with reviewer permissions
+Add-MailboxFolderPermission -Identity "calendar_owner_email:\Calendar" -User "calendar_user_email" -AccessRights Reviewer
+
+# Share a calendar from one user to another with limited permissions
+Add-MailboxFolderPermission -Identity "calendar_owner_email:\Calendar" -User "calendar_user_email" -AccessRights LimitedDetails
+
+# Remove calendar permissions for a specific calendar
+Remove-MailboxFolderPermission -Identity "calendar_owner_email:\Calendar" -User "calendar_user_email" -AccessRights FullAccess
+
+# Edit calendar permissions for a specific calendar
+Set-MailboxFolderPermission -Identity "calendar_owner_email:\Calendar" -User "calendar_user_email" -AccessRights ReadPermissions
+
+################################################################
+###### Distribution Group Operations
+################################################################
+
+# Get list of all distribution groups
 Get-DistributionGroup -ResultSize Unlimited
 
 # Get list of all members of a specific distribution group
@@ -27,135 +106,27 @@ Get-DistributionGroupMember -Identity "Group Name"
 # Add a member to a specific distribution group
 Add-DistributionGroupMember -Identity "Group Name" -Member "user@domain.com"
 
-#Get list of all mail contacts
-Get-MailContact -ResultSize Unlimited
+# Remove a member from a specific distribution group
+Remove-DistributionGroupMember -Identity "Group Name" -Member "user@domain.com"
 
-#Get list of all mail users
-Get-MailUser -ResultSize Unlimited
+################################################################
+###### Other Useful Operations
+################################################################
 
-#Get list of all shared mailboxes
-Get-Mailbox -RecipientTypeDetails SharedMailbox -ResultSize Unlimited
-
-#Get list of all resource mailboxes (room and equipment)
+# Get list of all resource mailboxes (room and equipment)
 Get-Mailbox -RecipientTypeDetails RoomMailbox, EquipmentMailbox -ResultSize Unlimited
 
-#Get list of all dynamic distribution groups
-Get-DynamicDistributionGroup -ResultSize Unlimited
-
-#Get list of all mail-enabled security groups
-Get-DistributionGroup -RecipientTypeDetails MailUniversalSecurityGroup, MailNonUniversalGroup -ResultSize Unlimited
-
-#Get list of all email address policies
-Get-EmailAddressPolicy
-
-#Get list of all accepted domains
-Get-AcceptedDomain
-
-#Get list of all remote domains
-Get-RemoteDomain
-
-#Get list of all transport rules
-Get-TransportRule
-
-#Get list of all mail flow connectors
-Get-TransportConnector
-
-#Get list of all mobile device mailbox policies
-Get-MobileDeviceMailboxPolicy
-
-#Get list of all retention policies
-Get-RetentionPolicy
-
-#Get list of all retention tags
-Get-RetentionPolicyTag
-
-#Get list of all mailbox audit log entries for a specific mailbox
-Search-MailboxAuditLog -Mailboxes
-
-#Get list of all mailbox permissions for a specific mailbox
-Get-MailboxPermission -Identity
-
-#Get list of all send-as permissions for a specific mailbox
-Get-RecipientPermission -Identity
-
-#Get list of all send-on-behalf permissions for a specific mailbox
-Get-Mailbox -Identity | Select-Object -ExpandProperty GrantSendOnBehalfTo   
-
-#Get list of all email forwarding settings for a specific mailbox
-Get-Mailbox -Identity | Select-Object -ExpandProperty ForwardingSmtpAddress, ForwardingAddress
-
-#Get list of all mailbox size and usage statistics
-Get-MailboxStatistics -Identity
-
-#Get list of all inactive mailboxes
+# Get list of all inactive mailboxes
 Get-Mailbox -InactiveMailboxOnly -ResultSize Unlimited
 
-#Get list of all litigation hold enabled mailboxes
+# Get list of all litigation hold enabled mailboxes
 Get-Mailbox -Filter {LitigationHoldEnabled -eq $true} -ResultSize Unlimited
 
 # Enable litigation hold for a specific mailbox
 Set-Mailbox -Identity "<mailbox_email>" -LitigationHoldEnabled $true -LitigationHoldDuration 365
-
-#Get list of all mailboxes with specific custom attribute
-Get-Mailbox -Filter {CustomAttribute1 -eq "Value"} -ResultSize Unlimited
-
-#Get list of all mailboxes with specific email address
-Get-Mailbox -ResultSize Unlimited | Where-Object { $_.EmailAddresses -like "*@domain.com*" }    
-
-#Get list of all mailboxes with specific mailbox size (e.g., greater than 1GB)
-Get-MailboxStatistics -ResultSize Unlimited | Where-Object { $_.TotalItemSize -gt 1GB } 
-
-#Get list of all mailboxes that have not been accessed in the last 90 days
-$cutoffDate = (Get-Date).AddDays(-90)
-Get-MailboxStatistics -ResultSize Unlimited | Where-Object { $_.LastLogonTime -lt $cutoffDate }     
-
-# Share a calendar from one user to another with specific permissions
-$calendarOwner = "<calendar_owner_email>:Calendar"
-$calendarUser = "<calendar_user_email>"
-Add-MailboxFolderPermission -Identity $calendarOwner -User $calendarUser -AccessRights FullAccess
-
-#Share a calendar from one user to another with reviewer permissions
-$calendarOwner = "<calendar_owner_email>:Calendar"
-$calendarUser = "<calendar_user_email>"
-Add-MailboxFolderPermission -Identity $calendarOwner -User $calendarUser -AccessRights Reviewer
-
-#Share a calendar from one user to another with limited permissions
-$calendarOwner = "<calendar_owner_email>:Calendar"
-$calendarUser = "<calendar_user_email>"
-Add-MailboxFolderPermission -Identity $calendarOwner -User $calendarUser -AccessRights LimitedDetails
-
-#Share a mailbox from one user to another with title and description permissions
-Add-MailboxPermission -Identity "<mailbox_email>" -User "<user_email>" -AccessRights ReadPermissions
-
-#Share a mailbox from one user to another with reviewer permissions
-Add-MailboxPermission -Identity "<mailbox_email>" -User "<user_email>" -AccessRights Reviewer
-
-#Share a mailbox from one user to another with specific permissions
-Add-MailboxPermission -Identity "<mailbox_email>" -User "<user_email>" -AccessRights FullAccess
-
-#Grant send-as permission to a user for a specific mailbox
-Add-RecipientPermission -Identity "<mailbox_email>" -Trustee "<user_email>" -AccessRights SendAs
-
-#Grant send-on-behalf permission to a user for a specific mailbox
-Set-Mailbox -Identity "<mailbox_email>" -GrantSendOnBehalfTo "<user_email>"
-
-#Grant send-on-behalf permission to a group for a specific mailbox
-Set-Mailbox -Identity "<mailbox_email>" -GrantSendOnBehalfTo "<group_email>"
-
-#Grant send-as permission to a group for a specific mailbox
-Add-RecipientPermission -Identity "<mailbox_email>" -Trustee "<group_email>" -AccessRights SendAs
-
-#Grant full access permission to a user for a specific mailbox
-Add-MailboxPermission -Identity "<mailbox_email>" -User "<user_email>" -AccessRights FullAccess
-
-#Grant full access permission to a group for a specific mailbox
-Add-MailboxPermission -Identity "<mailbox_email>" -User "<group_email>" -AccessRights FullAccess
 
 # Set automatic replies for a specific mailbox
 Set-MailboxAutoReplyConfiguration -Identity "<mailbox_email>" -AutoReplyState Enabled -InternalMessage "I am currently out of the office." -ExternalMessage "I am currently out of the office." 
 
 # Enable mailbox auditing for a specific mailbox
 Set-Mailbox -Identity "<mailbox_email>" -AuditEnabled $true
-
-# Set email forwarding for a specific mailbox
-Set-Mailbox -Identity "<mailbox_email>" -ForwardingSmtpAddress "forwardto@domain.com" -DeliverToMailboxAndForward $true
